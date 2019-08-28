180fe9f16623005137a80c5c1b05727155f73e34
TomonoriIshikawa <ishikawa@aidma-hd.jp>
2019-08-28 23:09:35 +0900

    AND演算を機械学習させてみる(LinearSVC)

--- and_ prediction.py
+++ and_ prediction.py
@@ -18,4 +18,4 @@ test_label = clf.predict(test_data)
 
 print(test_data , "の予測結果:" , test_label)
 
-print("正解率 = ", accuracy_score([0,0,0,1], test_label))
+print("正解率 = ", accuracy_score([0, 0, 0, 1], test_label))
