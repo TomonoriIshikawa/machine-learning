b0ce6e1cd69272fce6e6ed409af242597f2ab7aa
TomonoriIshikawa <ishikawa@aidma-hd.jp>
2019-08-28 22:00:30 +0900

    AND演算を機械学習させてみる

--- and_ prediction.py
+++ and_ prediction.py
@@ -2,7 +2,7 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 
 # 学習用データと結果の準備
-learn_data = [[0,0], [1,0], [0,1],[1,1]]
+learn_data = [[0,0], [1,0], [0,1], [1,1]]
 
 learn_label = [0, 0, 0, 1]
 
