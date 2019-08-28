94eecb156a3e83999aec1357dc5c3ab414cabeb0
TomonoriIshikawa <ishikawa@aidma-hd.jp>
2019-08-28 21:53:52 +0900

    Create and_ prediction.py
    AND演算を機械学習させて予測する。

---
+++ and_ prediction.py
@@ -0,0 +1,21 @@
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+
+# 学習用データと結果の準備
+learn_data = [[0,0], [1,0], [0,1],[1,1]]
+
+learn_label = [0, 0, 0, 1]
+
+# LinearSVCアルゴリズムを使う
+clf = LinearSVC()
+
+clf.fit(learn_data, learn_label)
+
+# テストデータによる予測
+test_data = [[0,0],[1,0],[0,1],[1,1]]
+
+test_label = clf.predict(test_data)
+
+print(test_data , "の予測結果:" , test_label)
+
+print("正解率 = ", accuracy_score([0,0,0,1], test_label))
